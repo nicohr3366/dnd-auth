f3ec66c99dc4a3fe9d1fc8ac8794b4fc4b176b9f
cuerdaso <alejandrorio0606@gmail.com>
2026-05-07 11:51:01 -0500

    Se agregaron a varios archivos la informacion relacionada con el modulo Gestion de Personajes

--- dnd_project/urls.py
+++ dnd_project/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(pattern_name='usuarios:lista'), name='home'),
     path('usuarios/', include('usuarios.urls')),
+    path('gestion_personajes/', include('gestion_personajes.urls')),
+
 ]
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Rol, PerfilUsuario
+from .models import Raza, Clase, Habilidad, Personaje
 
 
 @admin.register(Rol)
@@ -13,3 +14,8 @@ class PerfilUsuarioAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'rol')
     list_filter = ('rol',)
     search_fields = ('usuario__username',)
+
+admin.site.register(Raza)
+admin.site.register(Clase)
+admin.site.register(Habilidad)
+admin.site.register(Personaje)
